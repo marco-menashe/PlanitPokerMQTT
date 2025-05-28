d264b5b7a195eb2b121b3051e26ebbda65041ddc
Aidan Stutz <zebstrike1@hotmail.com>
2025-05-27 18:57:21 -0700

    added panels

--- planitpoker/src/main/java/com/example/T4B_DashboardPanel.java
+++ planitpoker/src/main/java/com/example/T4B_DashboardPanel.java
@@ -9,13 +9,23 @@ import java.awt.*;
  * @author javiergs
  */
 public class DashboardPanel extends JPanel {
-	
-	public DashboardPanel(DashboardNanny dashboardNanny) {
-		setLayout(new BorderLayout());
-		add(new CardsPanel(), BorderLayout.CENTER);
-		add(new SouthPanel(), BorderLayout.SOUTH);
-		add(new WestPanel(), BorderLayout.EAST);
-	}
-	
+    private SouthPanel southPanel;
+
+    public DashboardPanel(DashboardNanny dashboardNanny) {
+        setLayout(new BorderLayout());
+
+        CardsPanel cardsPanel = new CardsPanel(dashboardNanny);
+        dashboardNanny.setCardsPanel(cardsPanel);
+
+        southPanel = new SouthPanel();
+
+        add(cardsPanel, BorderLayout.CENTER);
+        add(southPanel, BorderLayout.SOUTH);
+        add(new WestPanel(dashboardNanny, this), BorderLayout.EAST);
+    }
+
+    public void updateResults(){
+        southPanel.updateResults();
+    }
 }
 
