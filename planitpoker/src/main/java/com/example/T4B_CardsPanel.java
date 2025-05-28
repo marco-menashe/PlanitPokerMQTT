7592219fb629747b825da4cc8b58023f04de650e
Aidan Stutz <zebstrike1@hotmail.com>
2025-05-27 18:55:43 -0700

    added card hover/look/resest

--- planitpoker/src/main/java/com/example/T4B_CardsPanel.java
+++ planitpoker/src/main/java/com/example/T4B_CardsPanel.java
@@ -12,20 +12,90 @@ import javax.swing.JPanel;
  * @author javiergs
  */
 public class CardsPanel extends JPanel {
-	
-	private static final String[] CARD_VALUES = {
-		"0", "½", "1", "2", "3", "5", "8", "20", "40", "10", "0", "?"
-	};
-	
-	public CardsPanel() {
-		setLayout(new GridLayout(4, 3, 10, 10));
-		for (String value : CARD_VALUES) {
-			JButton card = new JButton(value);
-			card.setBackground(new Color(172, 248, 199));
-			card.setFont(new Font("SansSerif", Font.BOLD, 20));
-			add(card);
-		}
-	}
+    private static final String[] CARD_VALUES = {
+            "0", "½", "1", "2", "3", "5", "8", "20", "40", "10", "0", "?"
+    };
 
+    private final DashboardNanny dashboardNanny;
+    private JButton selectedCard;
+    private static final Color DEFAULT_COLOR = new Color(172, 248, 199);
+    private static final Color HOVER_COLOR = new Color(200, 200, 200);
+    private static final Color SELECTED_COLOR = Color.YELLOW;
+    private static final Color CONFIRMED_COLOR = Color.GREEN;
+
+    public CardsPanel(DashboardNanny dashboardNanny) {
+        this.dashboardNanny = dashboardNanny;
+        setLayout(new GridLayout(4, 3, 10, 10));
+
+        for (String value : CARD_VALUES) {
+            JButton card = new JButton(value);
+            styleCard(card);
+            add(card);
+
+            card.addActionListener(e -> handleCardSelection(card));
+            addHoverEffect(card);
+        }
+    }
+
+    private void styleCard(JButton card) {
+        card.setBackground(DEFAULT_COLOR);
+        card.setFont(new Font("SansSerif", Font.BOLD, 20));
+        card.setFocusPainted(false);
+        card.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+    }
+
+    private void addHoverEffect(JButton card){
+        card.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent e){
+                if(!dashboardNanny.isVoteConfirmed() && card != selectedCard){
+                    card.setBackground(HOVER_COLOR);
+                }
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){
+                if(!dashboardNanny.isVoteConfirmed() && card != selectedCard){
+                    card.setBackground(DEFAULT_COLOR);
+                }
+            }
+        });
+    }
+
+    private void handleCardSelection(JButton card){
+        if(dashboardNanny.isVoteConfirmed()){
+            return;
+        }
+
+        if(selectedCard != null){
+            selectedCard.setBackground(DEFAULT_COLOR);
+        }
+
+        selectedCard = card;
+        card.setBackground(SELECTED_COLOR);
+        dashboardNanny.setCurrentVote(card.getText());
+    }
+
+    public void lockSelection(){
+        if(selectedCard != null){
+            selectedCard.setBackground(CONFIRMED_COLOR);
+        }
+        for(Component comp : getComponents()){
+            if(comp instanceof JButton){
+                comp.setEnabled(false);
+            }
+        }
+    }
+
+    public void resetCards(){
+        selectedCard = null;
+        for(Component comp : getComponents()){
+            if(comp instanceof JButton){
+                JButton btn = (JButton) comp;
+                btn.setEnabled(true);
+                btn.setBackground(DEFAULT_COLOR);
+            }
+        }
+    }
 }
 
