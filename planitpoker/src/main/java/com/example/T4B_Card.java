f31e77427dc0dea9e6643a86b72c63c351c3a24a
AdrianS2k <adriansancheza7@gmail.com>
2025-05-27 19:10:54 -0700

    changes to card class with attributes value and boolean for revealed

--- planitpoker/src/main/java/com/example/T4B_Card.java
+++ planitpoker/src/main/java/com/example/T4B_Card.java
@@ -1,3 +1,32 @@
-public class T4B_Card{
+package org.planit;
 
+public class T4B_Card {
+
+    private int value;
+    private boolean isRevealed;
+
+    public Card(int value) {
+        this.value = value;
+        this.isRevealed = false;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+
+    public void reveal() {
+        this.isRevealed = true;
+    }
+
+    public void hide() {
+        this.isRevealed = false;
+    }
 }
