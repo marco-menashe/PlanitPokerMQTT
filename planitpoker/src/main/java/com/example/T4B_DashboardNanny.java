e63b8fbe1a63ece81912cd4a5eafb49c9417fbc4
Aidan Stutz <zebstrike1@hotmail.com>
2025-05-27 18:56:41 -0700

    added voting mechanics

--- planitpoker/src/main/java/com/example/T4B_DashboardNanny.java
+++ planitpoker/src/main/java/com/example/T4B_DashboardNanny.java
@@ -7,11 +7,55 @@ package com.example;
  * @author javiergs
  */
 public class DashboardNanny {
-	
-	public DashboardNanny(Main main) {
-	
-		// ...
-		
-	}
-	
+    private String currentVote;
+    private boolean voteConfirmed = false;
+    private CardsPanel cardsPanel;
+
+    public DashboardNanny(){
+    }
+
+    public void setCardsPanel(CardsPanel cardsPanel){
+        this.cardsPanel = cardsPanel;
+    }
+
+    public void setCurrentVote(String vote){
+        if(!voteConfirmed){
+            currentVote = vote;
+        }
+    }
+
+    public void confirmVote(){
+        if(currentVote != null){
+            voteConfirmed = true;
+            try {
+                double numericVote = convertVoteToNumber(currentVote);
+                Blackboard.addVote(numericVote);
+                if(cardsPanel != null){
+                    cardsPanel.lockSelection();
+                }
+            } catch(NumberFormatException e){
+                System.out.println("Vote was not a number -> skipped");
+            }
+        }
+    }
+
+    public void startNewVote(){
+        voteConfirmed = false;
+        currentVote = null;
+        if(cardsPanel != null){
+            cardsPanel.resetCards();
+        }
+    }
+
+    public boolean isVoteConfirmed(){
+        return voteConfirmed;
+    }
+
+    private double convertVoteToNumber(String vote) throws NumberFormatException{
+        return switch (vote){
+            case "½" -> 0.5;
+            case "?" -> Double.NaN; //not sure what to do here, any number will affect avg score
+            default -> Double.parseDouble(vote);
+        };
+    }
 }
