275faab53c0cfaaf8336761bae21434d0f3c2ef2
AdrianS2k <adriansancheza7@gmail.com>
2025-05-28 19:19:38 -0700

    changed up repository and changed main for testing, unable to run

--- planitpoker/src/main/java/com/example/T4B_Main.java
+++ planitpoker/src/main/java/com/example/T4B_Main.java
@@ -9,13 +9,18 @@ import javax.swing.JFrame;
  *
  * /** */
 public class T4B_Main extends JFrame {
-	
+
 	public T4B_Main() {
 		T4B_LoginNanny loginNanny = new T4B_LoginNanny(this);
 		T4B_LoginPanel loginPanel = new T4B_LoginPanel(loginNanny);
 		add(loginPanel);
+
+		// 🔧 Add listener for debug/testing
+		T4B_Repository.getInstance().addPropertyChangeListener(evt -> {
+			System.out.println("REPO UPDATE: " + evt.getPropertyName() + " -> " + evt.getNewValue());
+		});
 	}
-	
+
 	public static void main(String[] args) {
 		T4B_Main main = new T4B_Main();
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +28,5 @@ public class T4B_Main extends JFrame {
 		main.setLocationRelativeTo(null);
 		main.setVisible(true);
 	}
-	
 }
 
